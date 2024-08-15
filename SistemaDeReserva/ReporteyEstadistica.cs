d8042f976c02d55d1fe4d5881723af923b261114
leo <leocolodro@gmail.com>
2024-08-15 01:56:08 -0300

    Agregue propiedades y constructores

--- SistemaDeReserva/ReporteyEstadistica.cs
+++ SistemaDeReserva/ReporteyEstadistica.cs
@@ -8,5 +8,16 @@ namespace SistemaDeReserva
 {
     internal class ReporteyEstadistica
     {
+        public DateTime FechaGeneracion { get; set; }
+        public string TipoDeReporte { get; set; }
+        public double DatosEstadisticos { get; set; }
+
+        public ReporteyEstadistica(DateTime fechaGeneracion, string tipoDeReporte, double datosEstadisticos)
+        {
+            FechaGeneracion = fechaGeneracion;
+            TipoDeReporte = tipoDeReporte;
+            DatosEstadisticos = datosEstadisticos;
+        }
+
     }
 }
--- SistemaDeReserva/Reserva.cs
+++ SistemaDeReserva/Reserva.cs
@@ -8,5 +8,15 @@ namespace SistemaDeReserva
 {
     internal class Reserva
     {
+        public DateTime Fecha { get; set; }
+        public string Nombre { get; set; }
+        public string TipoCancha { get; set; }
+
+        public Reserva(DateTime fecha, string nombre, string tipoCancha)
+        {
+            Fecha = fecha;
+            Nombre = nombre;
+            TipoCancha = tipoCancha;
+        }
     }
 }
